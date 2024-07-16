5fec197d4c00ef7d12e301cacbb1bf48ba1f3cb2
Mahdi008397 <Mahdi008397@gmail.com>
2024-07-16 10:17:47 +0330

    Update Assets 🎥

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,10 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello World!',
+            style: TextStyle(fontFamily: 'Sf'),
+          ),
         ),
       ),
     );
